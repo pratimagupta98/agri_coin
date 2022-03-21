5cbb76a31586f66ae483bed54b5c7355dc0b2d99
prandeveloper <87694761+prandeveloper@users.noreply.github.com>
2022-03-21 16:05:37 +0530

    amit

--- src/wrappers/category/CategoryFiveGrid.js
+++ src/wrappers/category/CategoryFiveGrid.js
@@ -1,115 +1,122 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Col, Container, Form, Input, Row,  } from "reactstrap";
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
+import { Col, Container, Form, Input, Row } from "reactstrap";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
 import { string } from "prop-types";
 
 const CategoryFiveGrid = ({ spaceBottomClass }) => {
-  
   return (
     <section className="st-p">
-    <Container>
+      <Container>
         <div className="bx1">
-         <Row>
-            <Col md="12" >
-             <ul className="st-2">
-                   <li className="st-1">
-                     <Link>
-                       <i className="pe-7s-cash circle-1" />
-                       <p>Fast Recharge</p>
-                     </Link>
-                    </li>
-                    <li className="st-1">
-                     <Link >
-                       <i className="pe-7s-global circle-1" />
-                       <p>F. Internet Bill</p>
-                     </Link>
-                    </li>
-                    <li className="st-1">
-                     <Link>
-                       <i className="pe-7s-global circle-1" />
-                       <p>Internet Bill</p>
-                     </Link>
-                    </li>
-                    <li className="st-1">
-                     <Link>
-                       <i className="pe-7s-phone circle-1" />
-                       <p>M. Balance</p>
-                     </Link>
-                    </li>
-                    <li className="st-1">
-                     <Link to={process.env.PUBLIC_URL + "/gass"}> {string["electricity"]} 
-                       <i className="pe-7s-safe circle-1" />
-                       <p>Gass</p>
-                     </Link>
-                    </li>
-                    <li className="st-1">
-                     <Link to={process.env.PUBLIC_URL + "/electricity"}>{string["electricity"]}
-                       <i className="pe-7s-light circle-1" />
-                       <p>Electricity</p>
-                     </Link>
-                    </li>
-                    <li className="st-1">
-                     <Link to={process.env.PUBLIC_URL + "/internetbill"}>{string["internetbill"]}
-                       <i className="pe-7s-airplay circle-1" />
-                       <p>TV</p>
-                     </Link>
-                    </li>
-                    <li className="st-1">
-                     <Link>
-                       <i className="pe-7s-shopbag circle-1" />
-                       <p>Shopping Bill</p>
-                     </Link>
-                    </li>
-                    <li className="st-1">
-                     <Link>
-                       <i className="pe-7s-clock circle-1" />
-                       <p>Airtime</p>
-                     </Link>
-                    </li>
-                    <li className="st-1">
-                     <Link>
-                       <i className="pe-7s-more circle-1" />
-                       <p> More</p>
-                     </Link>
-                    </li>
-                   
-               </ul>
-            </Col> 
-              <div className="bx-2">
+          <Row>
+            <Col md="12">
+              <ul className="st-2">
+                <li className="st-1">
+                  <Link>
+                    <i className="pe-7s-cash circle-1" />
+                    <p>Fast Recharg</p>
+                  </Link>
+                </li>
+                <li className="st-1">
+                  <Link>
+                    <i className="pe-7s-global circle-1" />
+                    <p>F. Internet Bill</p>
+                  </Link>
+                </li>
+                <li className="st-1">
+                  <Link>
+                    <i className="pe-7s-global circle-1" />
+                    <p>Internet Bill</p>
+                  </Link>
+                </li>
+                <li className="st-1">
+                  <Link>
+                    <i className="pe-7s-phone circle-1" />
+                    <p>M. Balance</p>
+                  </Link>
+                </li>
+                <li className="st-1">
+                  <Link to={process.env.PUBLIC_URL + "/gass"}>
+                    {" "}
+                    {string["electricity"]}
+                    <i className="pe-7s-safe circle-1" />
+                    <p>Gass</p>
+                  </Link>
+                </li>
+                <li className="st-1">
+                  <Link to={process.env.PUBLIC_URL + "/electricity"}>
+                    {string["electricity"]}
+                    <i className="pe-7s-light circle-1" />
+                    <p>Electricity</p>
+                  </Link>
+                </li>
+                <li className="st-1">
+                  <Link to={process.env.PUBLIC_URL + "/internetbill"}>
+                    {string["internetbill"]}
+                    <i className="pe-7s-airplay circle-1" />
+                    <p>TV</p>
+                  </Link>
+                </li>
+                <li className="st-1">
+                  <Link>
+                    <i className="pe-7s-shopbag circle-1" />
+                    <p>Shopping Bill</p>
+                  </Link>
+                </li>
+                <li className="st-1">
+                  <Link>
+                    <i className="pe-7s-clock circle-1" />
+                    <p>Airtime</p>
+                  </Link>
+                </li>
+                <li className="st-1">
+                  <Link>
+                    <i className="pe-7s-more circle-1" />
+                    <p> More</p>
+                  </Link>
+                </li>
+              </ul>
+            </Col>
+            <div className="bx-2">
               <Form>
                 <div className="int-box">
-                  <FormControlLabel control={<Checkbox defaultChecked />} label="Prepaid" />
+                  <FormControlLabel
+                    control={<Checkbox defaultChecked />}
+                    label="Prepaid"
+                  />
                 </div>
                 <div className="int-box">
-                  <FormControlLabel control={<Checkbox  />} label="Postpaid" />
-                </div> 
-              <Row>
-                        <Col md="3">
-                            <Input type="text"
-                             className="form-control mb-st"
-                             placeholder="Enter Mobile Number "
-                             >
-                            </Input>
-                         </Col>
-                         <Col md="3">
-                         <Input type="text" className="form-control mb-st" ></Input>
-                         </Col>
-                 <Col md="3">
-                    <Input type="text" className="form-control mb-st" ></Input>
+                  <FormControlLabel control={<Checkbox />} label="Postpaid" />
+                </div>
+                <Row>
+                  <Col md="3">
+                    <Input
+                      type="text"
+                      className="form-control mb-st"
+                      placeholder="Enter Mobile Number "
+                    ></Input>
                   </Col>
-                 <Col md="3">
-                    <button type="button" class="btn-s mb-st">Continue</button>
-                 </Col>
-              </Row>
+                  <Col md="3">
+                    <Input type="text" className="form-control mb-st"></Input>
+                  </Col>
+                  <Col md="3">
+                    <Input type="text" className="form-control mb-st"></Input>
+                  </Col>
+                  <Col md="3">
+                    <button type="button" class="btn-s mb-st">
+                      Continue
+                    </button>
+                  </Col>
+                </Row>
               </Form>
-              </div>
-         </Row>
-         </div>
-    </Container>
-</section>
+            </div>
+          </Row>
+        </div>
+      </Container>
+    </section>
   );
 };
 
